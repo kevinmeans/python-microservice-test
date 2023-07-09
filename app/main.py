bdf3a27f891e96f1d70aa50e1de6e4df1467d36e
Kevin Means <kevin.means@bridgerphotonics.com>
2023-07-09 10:56:00 -0700

    Pylint changes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from typing import Optional
-
 from fastapi import FastAPI
+
 #https://github.com/patrickloeber/python-docker-tutorial/tree/main/example2
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    # Doc String 1
     return {"HelloO": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, question: Optional[str] = None):
+    # Doc String 1
+    return {"item_id": item_id, "q": question}
