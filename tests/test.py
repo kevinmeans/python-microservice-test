fc2e5f24769b80fd19612262379eee2d0df8daf4
Kevin Means <kevmeans@gmail.com>
2024-04-05 14:12:28 -0700

    test-prod

--- tests/test.py
+++ tests/test.py
@@ -21,7 +21,7 @@ class Testing(unittest.TestCase):
         Simple test case to check if a Goodeye is returned
         """
         response = client.get("/")
-        self.assertEqual(response.json(), {"Helllo": "World!"})
+        self.assertEqual(response.json(), {"XXXXX": "World!"})
 
 
 if __name__ == '__main__':
